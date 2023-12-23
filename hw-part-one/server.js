875d4c44911627abcc3bcc7fee9583466fc80d72
William Kostreski <wkostreski@gmail.com>
2023-12-23 16:28:47 -0500

    tip calculator added

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -6,8 +6,20 @@ app.listen(PORT, () => {
     console.log('connected Daddy')
 })
 
+// Greeting added
 app.get('/greeting/:name', (req, res) => {
     const username = req.params.name
     res.send(`Wow! Hello there, ${username}`)
 })
 
+// Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send (`<h1> ${req.params.total * (req.params.tipPercentage * .01)} </h1>`)
+})
+
+// Magic 8 Ball
+
+app.get('/greeting/:name', (req, res) => {
+    const username = req.params.name
+    res.send(`Wow! Hello there, ${username}`)
+})
