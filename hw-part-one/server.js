4e3cc1f4c7d81bac51d91c23332d9fcd17a338dd
William Kostreski <wkostreski@gmail.com>
2023-12-23 16:55:01 -0500

    magic 8 ball added

--- hw-part-one/server.js
+++ hw-part-one/server.js
@@ -19,7 +19,11 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 
 // Magic 8 Ball
 
-app.get('/greeting/:name', (req, res) => {
-    const username = req.params.name
-    res.send(`Wow! Hello there, ${username}`)
+app.get('/magic/:question', (req, res) => {
+    const question = req.params.question.replace(/%20/g, ' ')
+    const potentialAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+    const response = potentialAnswers [Math.floor (Math.random() * potentialAnswers.length)]
+    res.send (`Question: ${question} Answer: ${response}`)
 })
+
